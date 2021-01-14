2f2064be09237bfa563e5fc8695b6335a0cd1d3b
Sannim <inegg.apps@gmail.com>
2021-01-14 21:33:26 +0900

    사용자 인증 컴포넌트 구성하기

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const onChange = (event) => {
+        const {
+            target: { name, value },
+        } = event;
+        if (name === 'email') {
+            setEmail(value);
+        } else if (name === 'password') {
+            setPassword(value);
+        }
+        console.log(event.target.name);
+    };
+    const onSubmit = (event) => {
+        event.preventDefault();
+        console.log('onSubmit');
+    };
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input
+                    type='text'
+                    name='email'
+                    placeholder='Email'
+                    required
+                    value={email}
+                    onChange={onChange}
+                />
+                <input
+                    type='password'
+                    name='password'
+                    placeholder='Password'
+                    required
+                    value={password}
+                    onChange={onChange}
+                />
+                <input type='submit' value='Login' />
+            </form>
+            <div>
+                <button>Continue with Google</button>
+                <button>Continue with Github</button>
+            </div>
+        </div>
+    );
+};
 export default Auth;
