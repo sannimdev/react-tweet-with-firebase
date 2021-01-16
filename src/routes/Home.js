1660df0f8aea1d0af8dcbf929907cb8aa3cde51a
Sannim <inegg.apps@gmail.com>
2021-01-16 13:37:28 +0900

    Home 컴포넌트 구성 및 firebase (firestore) 사용 준비하기

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default () => <span>Home</span>;
+export default () => {
+    const [nweet, setNweet] = useState('');
+    const onSubmit = (event) => {
+        event.preventDefault();
+    };
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setNweet(value);
+    };
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input
+                    value={nweet}
+                    onChange={onChange}
+                    type='text'
+                    placeholder="What's on your mind?"
+                    maxLength={120}
+                />
+                <input type='submit' value='Nweet' />
+            </form>
+        </div>
+    );
+};
