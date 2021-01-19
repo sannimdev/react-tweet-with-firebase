a9db59f41b398e2202444c7cceae6a3552a25e60
Sannim <inegg.apps@gmail.com>
2021-01-19 19:18:55 +0900

    게시글 작성자에게만 삭제, 수정 버튼 보이게 만들기
    * Nweet 게시글 컴포넌트 별도 분리

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,4 @@
+import Nweet from 'components/Nweet';
 import { dbServce } from 'fbase';
 import React, { useEffect, useState } from 'react';
 
@@ -46,9 +47,11 @@ export default ({ userObj }) => {
             </form>
             <div>
                 {nweets.map((nweet) => (
-                    <div key={nweet.id}>
-                        <h4>{nweet.text}</h4>
-                    </div>
+                    <Nweet
+                        key={nweet.id}
+                        nweetObj={nweet}
+                        isOwner={nweet.creatorId === userObj.uid}
+                    />
                 ))}
             </div>
         </div>
