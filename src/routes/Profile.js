fbf25b61b66c33c8764f55e948f4443354364773
Sannim <inegg.apps@gmail.com>
2021-01-24 20:16:20 +0900

    내가 쓴 Nweets 보기 (정렬X)
    * firestore에서 색인 설정이 필요하다. 설정 링크는 콘솔창 확인.

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import { authService } from 'fbase';
+import React, { useEffect } from 'react';
+import { authService, dbServce } from 'fbase';
 import { useHistory } from 'react-router-dom';
 
-export default () => {
+export default ({ userObj }) => {
     const history = useHistory();
     const onLogOutClick = () => {
         authService.signOut();
         history.push('/');
     };
+
+    const getMyNweets = async () => {
+        const nweets = await dbServce
+            .collection('nweets')
+            .where('creatorId', '==', userObj.uid)
+            // .orderBy('createdAt', 'desc') 색인이 생성된 이후에 사용할 수 있다.
+            .get();
+        console.log(nweets.docs.map((doc) => doc.data()));
+    };
+
+    useEffect(() => {
+        getMyNweets();
+    }, []);
 
     return (
         <>
